75ec2bdb8b83831e47df1338637dbc760c3af0fc
Srhiulli <srhiulli@gmail.com>
2024-09-29 13:01:51 -0300

    add http status, and returns

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 app.post('/sign-up', async (req, res) => {
   const { username, avatar } = req.body;
   if (!username || !avatar) {
-    return res.status(400).json({
+    return res.status(204).json({
       message: "Todos os campos são obrigatórios!"
     });
   }
@@ -44,15 +44,22 @@ app.post('/sign-up', async (req, res) => {
     return rowCount > 0; 
   }
   if (await hasUser(username)) return res.status(409).send("Usuário já cadastrado!");
-  await client.query('INSERT INTO users (username, avatar) VALUES ($1, $2) RETURNING *', [username, avatar])
-  res.status(200).send('Novo usuário cadastrado')
-
+  const { rows }  = await client.query('INSERT INTO users (username, avatar) VALUES ($1, $2) RETURNING *', [username, avatar])
+  res.status(201).json({ 
+    message: "Novo usuário cadastrado com sucesso",
+    body: { 
+      username : username,
+      avatar: avatar, 
+      id: rows[0].id,
+      created_at: rows[0].created_at
+     }
+  });
 });
 
 app.post('/tweets', async (req, res) => {
   const { username, tweet } = req.body;
   if (!username || !tweet) {
-    return res.status(400).json({
+    return res.status(204).json({
       message: "Todos os campos são obrigatórios!"
     });
   }
@@ -65,36 +72,61 @@ app.post('/tweets', async (req, res) => {
     }
   const userInfo = await getUser(username);
   if (!userInfo.exists) {
-    return res.status(409).send("Usuário não encontrado!");
+    return res.status(409).json({
+      message: "Usuário não encontrado!",
+    });
   } 
     const userData = userInfo.user
-    console.log(userData);
-    await client.query(
-      'INSERT INTO tweets (users_id, tweet) VALUES ($1, $2)',
+   await client.query(
+      'INSERT INTO tweets (user_id, tweet) VALUES ($1, $2)',
       [userData.id, tweet]
     );
-    return res.status(200).json({ message: "Tweet enviado com sucesso!" });
+    return res.status(201).json({ 
+      message: "Tweet enviado com sucesso!", 
+      body: {
+        username: username,
+        tweet: tweet,
+        avatar: userData.avatar, 
+      }
+    });
 });
 
-app.get('/tweets', (req, res) => { 
-  const lastTenTweets = tweets.slice(-10); 
-  res.send(lastTenTweets);
+app.get('/tweets', async  (req, res) => { 
+  const {rows} = await client.query('SELECT users.username, users.avatar, tweets.tweet, tweets.created_at FROM tweets JOIN users ON tweets.user_id = users.id ORDER BY tweets.created_at DESC LIMIT 10');
+  res.json(
+    rows.map(row => ({
+      username: row.username,
+      avatar: row.avatar,
+      tweet: row.tweet,
+      created_at: row.created_at
+    }))
+  );
 });
+
+
+
+app.get('/tweets/:username', async (req, res) => { 
+  const { username } = req.params; 
+  const { rows, rowCount } = await client.query(
+    `SELECT users.username, users.avatar, tweets.tweet, tweets.created_at 
+     FROM tweets 
+     JOIN users ON tweets.user_id = users.id 
+     WHERE users.username ILIKE $1 
+     ORDER BY tweets.created_at DESC 
+     LIMIT 10`,
+    [username] 
+  );
+if (rowCount === 0) return res.status(404).json({ message : "Usuário ou tweets não encontrados!"})
+  res.status(200).json(
+    rows.map(row => ({
+      username: row.username,
+      avatar: row.avatar,
+      tweet: row.tweet,
+      created_at: row.created_at
+    }))
+  )
+})
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
 });
-
-app.get('/tweets/:username', (req, res) => { 
-  const { username } = req.params; 
-
-  function getUserTweets(username) {
-    return tweets.filter(tweet => tweet.username === username); 
-  }
-
-  const userTweets = getUserTweets(username);
-    res.send(userTweets); 
-
-});
-
-
